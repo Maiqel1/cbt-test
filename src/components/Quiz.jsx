a5d9ae48e8b33519c426241d657a65c227e1e946
maiqel <michaelolowe321@gmail.com>
2023-01-18 15:11:42 +0100

    feat: fully implemented timer, added auto submit functionality when timer runs out e.t.c

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,30 +1,49 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Questions } from "../Question/QuestionBank";
 import { QuizContext } from "../Context/QuizContext";
 
 function Quiz() {
   const { score, setScore, setExamState } = useContext(QuizContext);
 
-// setting question and answer state
+  // setting question and answer state
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
 
-//   this state is to clear the radio inputs when next is clicked, and retain state when previous is clicked
+  //   setting timer state
+  // const [timeLeft, setTimeLeft] = useState(100);
+  const timeAllocated = 10;
+  const [timeRemaining, setTimeRemaining] = useState(timeAllocated);
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(0);
+      finishQuiz(false);
+    }
+  }, [timeRemaining]);
+
+  //   this state is to clear the radio inputs when next is clicked, and retain state when previous is clicked
   const [answers, setAnswers] = useState({});
 
   const handleChange = (currentQuestion, optionChosen) => {
     setAnswers({ ...answers, [currentQuestion]: optionChosen });
   };
 
-
-//   next question and score increment
+  //   next question and score increment
   const nextQuestion = () => {
     // theres a flaw here, score increments once answer is picked, and doesnt reduce when answer is changed to incorrect one.
     // possible fix? disable previous question button(not very feasible)
     // alternatively,move score increment to finish quiz function
     // if (Questions[currentQuestion].answer === optionChosen) {
     //   setScore(score + 1);
-    // } 
+    // }
     // this condition returns the user to the first question when he clicks "next question" while on the last question
     if (currentQuestion === Questions.length - 1) {
       setCurrentQuestion(0);
@@ -34,7 +53,7 @@ function Quiz() {
     setAnswers({ ...answers });
   };
 
-    // navigation to previous questions
+  // navigation to previous questions
   const previousQuestion = () => {
     if (currentQuestion === 0) {
       setCurrentQuestion(0);
@@ -42,39 +61,60 @@ function Quiz() {
       setCurrentQuestion(currentQuestion - 1);
     }
     // const prevAnswer = answers[currentQuestion - 1];
-
   };
 
-//   code to submit quiz, needs work
-//   const finishQuiz = () => {
-//     // if (Questions[currentQuestion].answer === optionChosen) {
-//     //   setScore(score + 1);
-//     // }
+  //   code to submit quiz, needs work
+  //   const finishQuiz = () => {
+  //     // if (Questions[currentQuestion].answer === optionChosen) {
+  //     //   setScore(score + 1);
+  //     // }
 
-//     setExamState("EndScreen");
-//   };
+  //     setExamState("EndScreen");
+  //   };
 
-const finishQuiz = () => {
-    if(window.confirm("Are you sure you want to submit?")){
-      let score = 0;
-      for(let i=0; i<Questions.length; i++){
-        if(answers[i] === Questions[i].answer){
-          score++;
+  // setting timer state
+
+  //   const finishQuiz = () => {
+  //     if (window.confirm("Are you sure you want to submit?")) {
+  //       let score = 0;
+  //       for (let i = 0; i < Questions.length; i++) {
+  //         if (answers[i] === Questions[i].answer) {
+  //           score++;
+  //         }
+  //       }
+  //       setScore(score);
+  //       setExamState("EndScreen");
+  //     }
+  //   };
+
+  const finishQuiz = (showPrompt = true) => {
+    if (showPrompt) {
+      if (window.confirm("Are you sure you want to submit?")) {
+        let score = 0;
+        for (let i = 0; i < Questions.length; i++) {
+          if (answers[i] === Questions[i].answer) {
+            score++;
+          }
         }
+        setScore(score);
+        setExamState("EndScreen");
       }
-      setScore(score);
+    } else {
       setExamState("EndScreen");
     }
   };
 
-
   return (
     <div className="Quiz">
+      <h1>
+        Time Left: {Math.floor(timeRemaining / 60)} : {timeRemaining % 60}
+      </h1>
 
-        <div>
+      <div>
+        {timeRemaining <= timeAllocated / 2 && (
           <button onClick={finishQuiz}>Finish Quiz</button>
-        </div>
-
+        )}
+      </div>
 
       <h1>{Questions[currentQuestion].prompt}</h1>
 
@@ -138,7 +178,6 @@ const finishQuiz = () => {
               handleChange(currentQuestion, "D");
             }}
             checked={answers[currentQuestion] === "D"}
-
           />
           <label htmlFor="optionD">{Questions[currentQuestion].optionD}</label>
         </div>
@@ -147,7 +186,7 @@ const finishQuiz = () => {
       <div className="nextPrev">
         <button onClick={previousQuestion}>Previous Question</button>
 
-          <button onClick={nextQuestion}>Next Question</button>
+        <button onClick={nextQuestion}>Next Question</button>
       </div>
     </div>
   );
