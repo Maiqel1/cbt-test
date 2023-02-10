50bc7f0272914cb2e52f479e56e4155263b617c3
maiqel <michaelolowe321@gmail.com>
2023-02-10 14:12:45 +0100

    feat: added page reload after logout, styled endscreen component

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,35 +1,43 @@
-import { useContext, useEffect } from "react"
-import QuizContext from "../Context/QuizContext"
+import { useContext, useEffect } from "react";
+import QuizContext from "../Context/QuizContext";
 // import { Questions } from "../Question/QuestionBank";
-import "../App.css"
+import "../App.css";
 
 function EndScreen() {
-
-  const { score, setScore, setExamState, Questions, setTimeRemaining, setAnswers, timeAllocated, completedCourses, setCompletedCourses, selectedCourse, setCurrentQuestion } = useContext(QuizContext);
+  const {
+    score,
+    setScore,
+    setExamState,
+    Questions,
+    setTimeRemaining,
+    setAnswers,
+    timeAllocated,
+    completedCourses,
+    setCompletedCourses,
+    selectedCourse,
+    setCurrentQuestion,
+  } = useContext(QuizContext);
   useEffect(() => {
     if (!completedCourses.includes(selectedCourse)) {
       setCompletedCourses([...completedCourses, selectedCourse]);
     }
-  }, [])
+  }, []);
   const restartQuiz = () => {
-
     setTimeRemaining(timeAllocated);
     setAnswers([]);
     setScore(0);
-    setExamState('menu')
-  }
-
+    setExamState("menu");
+  };
 
   return (
-    <div className="EndScreen">
-      <h1>Exam Finished</h1>
-      {/* <h3>{score} / {Questions.length}</h3>
-
-      <p>{Math.round(score / Questions.length * 100)}%</p> */}
-
-      <button onClick={restartQuiz}>Go to next course</button>
-
+    <div className="EndScreen mt-5 pt-5">
+      <div className="mx-auto text-center  endscreen-card">
+        <h4 className="pt-5">Exam submitted Successfully</h4>
+        <h4 className="pt-5 pb-5">Go to the next course to proceed with your exam
+Proceed to next course?</h4>
+        <button onClick={restartQuiz} className="btn text-white p-3">Go to next course</button>
+      </div>
     </div>
-  )
+  );
 }
-export default EndScreen
+export default EndScreen;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,36 +1,48 @@
-  import {useContext, useState} from 'react'
-  import QuizContext from "../Context/QuizContext"
-  import {useLocation, useNavigate} from "react-router-dom"
+import { useContext, useState, useEffect } from "react";
+import QuizContext from "../Context/QuizContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
-  function Nav() {
+function Nav() {
+  let location = useLocation();
+  let navigate = useNavigate();
 
-    let location = useLocation();
-    let navigate = useNavigate();
+  const studentData = JSON.parse(localStorage.getItem("student"));
+  const [student, setStudent] = useState(studentData || {});
 
-    const studentData = JSON.parse(localStorage.getItem('student'))
-    const [student, setStudent] = useState(studentData || {})   
-    
-    const logOut = () => {
-      localStorage.removeItem('authtoken');
-    localStorage.removeItem('student');
-    localStorage.removeItem('results');
+  const logOut = () => {
+    localStorage.removeItem("authtoken");
+    localStorage.removeItem("student");
+    localStorage.removeItem("results");
 
-    navigate('/login')
+    navigate("/login");
+    window.location.reload(false);
+  };
 
-    }
+      
 
+  const { timeAllocated, timeRemaining, handleFinish, completedCourses } =
+    useContext(QuizContext);
 
-    const {timeAllocated, timeRemaining, handleFinish, completedCourses} = useContext(QuizContext)
+  return (
+    <div className="navBar">
+      <div className="d-flex justify-content-between container pt-2">
+        {student.fullname}
 
-    return (
-      <div className="navBar">
-          <div className="d-flex justify-content-between container pt-2">
-            {student.fullname}
-            
-            {timeRemaining <= timeAllocated / 1.3 &&  location.pathname ==="/quiz" ? (<p onClick={handleFinish} className="submit-button"> SUBMIT</p>) : location.pathname === "/results" || completedCourses.length ===  5 ? (<p onClick={logOut} className="logout-button">LOGOUT</p>): <p></p>}
-            
-          </div>
+        {timeRemaining <= timeAllocated / 1.3 &&
+        location.pathname === "/quiz" ? (
+          <p onClick={handleFinish} className="submit-button">
+            SUBMIT
+          </p>
+        ) : location.pathname === "/results" ||
+          completedCourses.length === 5 ? (
+          <p onClick={logOut} className="logout-button">
+            LOGOUT
+          </p>
+        ) : (
+          <p></p>
+        )}
       </div>
-    )
-  }
-  export default Nav
+    </div>
+  );
+}
+export default Nav;
